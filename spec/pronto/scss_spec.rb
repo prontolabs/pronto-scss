da3f49832ffc7890827891daa34c84b5cab158ae
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-05-31 09:56:47 +0300

    Add a simple integration spec

--- spec/pronto/scss_spec.rb
+++ spec/pronto/scss_spec.rb
@@ -16,6 +16,23 @@ module Pronto
         let(:patches) { [] }
         it { should == [] }
       end
+
+      context 'patches with a two smells' do
+        include_context 'test repo'
+
+        let(:patches) { repo.diff('master') }
+
+        its(:count) { should == 2 }
+        its(:'first.msg') do
+          should ==
+            'Line should be indented 2 spaces, but was indented 4 spaces'
+        end
+
+        its(:'last.msg') do
+          should ==
+            'Line should be indented 2 spaces, but was indented 4 spaces'
+        end
+      end
     end
   end
 end
