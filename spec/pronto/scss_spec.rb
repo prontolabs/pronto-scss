dc53dfa9e8de98b00391b3d8e7b3166e438dabca
Markus Doits <markus.doits@stellenticket.de>
2016-02-09 17:02:53 +0100

    Add tests for `scss_files` option
    * Added a second file to lint with different errors

--- spec/pronto/scss_spec.rb
+++ spec/pronto/scss_spec.rb
@@ -17,12 +17,13 @@ module Pronto
         it { should == [] }
       end
 
-      context 'patches with a two smells' do
+      context 'patches with a three smells' do
         include_context 'test repo'
 
         let(:patches) { repo.diff('master') }
 
-        its(:count) { should == 2 }
+        its(:count) { should == 3 }
+
         its(:'first.msg') do
           should ==
             'Line should be indented 2 spaces, but was indented 4 spaces'
@@ -30,7 +31,30 @@ module Pronto
 
         its(:'last.msg') do
           should ==
-            'Line should be indented 2 spaces, but was indented 4 spaces'
+            'Each selector in a comma sequence should be on its own single line'
+        end
+
+        context 'only one file with one smell included by `scss_files` option' do
+          before(:all) do
+            File.open('spec/.scss-lint.yml', 'w') do |file|
+              file.write "scss_files: '**/world*scss'\n"
+            end
+          end
+
+          after(:all) { FileUtils.rm('spec/.scss-lint.yml') }
+
+          before do
+            scss.should_receive(:config) do
+              SCSSLint::Config.load('spec/.scss-lint.yml')
+            end.at_least(:once)
+          end
+
+          its(:count) { should == 1 }
+
+          its(:'first.msg') do
+            should ==
+              'Each selector in a comma sequence should be on its own single line'
+          end
         end
       end
     end
